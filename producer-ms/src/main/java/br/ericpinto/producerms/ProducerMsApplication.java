1eb7df095bd913ee671b4c88be273862d16efed8
Éric Pinto <ericgrandopinto@gmail.com>
2021-07-27 23:18:19 -0300

    initial commit

---
+++ producer-ms/src/main/java/br/ericpinto/producerms/ProducerMsApplication.java
@@ -0,0 +1,13 @@
+package br.ericpinto.producerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerMsApplication.class, args);
+    }
+
+}
