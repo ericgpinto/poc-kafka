347e806e1749263654fbe442082b14aac2854d22
Éric Pinto <ericgrandopinto@gmail.com>
2021-07-29 10:57:29 -0300

    some settings

---
+++ producer-ms/src/main/java/br/ericpinto/producerms/model/UserDTO.java
@@ -0,0 +1,17 @@
+package br.ericpinto.producerms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDTO {
+
+    private String id;
+    private String name;
+    private Double[] coordinates;
+}
